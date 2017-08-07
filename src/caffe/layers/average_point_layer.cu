f386d52f55be6b0e565094fc6472e6f157c134e9
JiajianZeng <503917315@qq.com>
2017-08-07 22:37:25 +0800

    Add propagate_down support for average_point_layer

--- src/caffe/layers/average_point_layer.cu
+++ src/caffe/layers/average_point_layer.cu
@@ -23,15 +23,16 @@ template <typename Dtype>
 void AveragePointLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top,
                                                          const vector<bool>& propagate_down,
                                                          const vector<Blob<Dtype>*>& bottom) {
-   for (int b = 0; b < bottom[0]->num(); ++b){    
-    const Dtype alphax = top[0]->cpu_diff()[top[0]->offset(b, 0)];
-    const Dtype alphay = top[0]->cpu_diff()[top[0]->offset(b, 1)];
-    for (int n = 0; n < num_landmark_; ++n){
-      bottom[0]->mutable_cpu_diff()[bottom[0]->offset(b, n)] = alphax/num_landmark_;
-      bottom[0]->mutable_cpu_diff()[bottom[0]->offset(b, n, 1)] = alphay/num_landmark_;
+  if (propagate_down[0]){
+    for (int b = 0; b < bottom[0]->num(); ++b){    
+      const Dtype alphax = top[0]->cpu_diff()[top[0]->offset(b, 0)];
+      const Dtype alphay = top[0]->cpu_diff()[top[0]->offset(b, 1)];
+      for (int n = 0; n < num_landmark_; ++n){
+        bottom[0]->mutable_cpu_diff()[bottom[0]->offset(b, n)] = alphax/num_landmark_;
+        bottom[0]->mutable_cpu_diff()[bottom[0]->offset(b, n, 1)] = alphay/num_landmark_;
+      }
     }
   }
-
 }
 
 INSTANTIATE_LAYER_GPU_FUNCS(AveragePointLayer);
